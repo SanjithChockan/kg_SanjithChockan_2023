d7589791fad61c3cf5ae9fe09b0cf84bf071a9cd
Sanjith Chockan <sanjith@Sanjiths-MacBook-Air.local>
2021-04-28 00:15:22 -0400

    Implemented Solution

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var inputs = ["3", "25", "209"]
+var phonetic = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+var output = []
+
+for (let i = 0; i < inputs.length; i++) {
+    let tempString = "";
+    let indInput = inputs[i];
+    for (let k = 0; k < indInput.length; k++) {
+        tempString += phonetic[parseInt(indInput[k])];
+    }
+    output.push(tempString);
+}
+
+console.log(output.toString());
